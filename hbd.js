63f764acac08a715fd2a868ca5167c364fa7423b
thecoderash <90136441+thecoderash@users.noreply.github.com>
2022-02-01 21:51:04 +0530

    Add files via upload

---
+++ hbd.js
@@ -0,0 +1,48 @@
+var x = document.getElementById("hbd");
+var y = "happy birthday grayblade bitch";
+var i = 0;
+var m="";
+
+
+function randchar() {
+    return String.fromCharCode(97 + Math.floor(Math.random() * 26));
+}
+
+var Iid;
+
+var intervalId = null;
+var varCounter = 0;
+function varName() {
+    if (x.innerHTML !== y){
+        if(x.innerHTML===""){
+               x.innerHTML+=randchar();
+           }
+        else if(i===5 || i===14 || i===24){
+            x.innerHTML=m;
+            x.innerHTML+=" ";
+            m=x.innerHTML;
+            i++;
+        }
+       else if(x.innerHTML[i]!==y[i])
+       {
+           x.innerHTML=m;
+           x.innerHTML+=randchar();
+       }
+       else
+       {
+        console.log('2');
+            m=x.innerHTML;
+           x.innerHTML+=randchar();
+           i++;
+       }
+    console.log(x.innerHTML);
+    }
+    else {
+
+        setTimeout(function() { location.replace('https://youtube.com/shorts/lrm7tq-qVEs?feature=share'); }, 2000);
+        clearInterval(intervalId);
+    }
+};
+
+intervalId = setInterval(varName, 5);
+
